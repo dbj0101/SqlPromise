b5300b50deedd044fb5a5559ff31210544a87c7a
DB <100801952+dbj0101@users.noreply.github.com>
2023-10-19 07:37:53 -0400

    Update connectionInfo.js

--- connectionInfo.js
+++ connectionInfo.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 
-// const connMap = new Map();
-// for(conn of JSON.parse(process.env.CONNECTION_STRINGS)) {
-//     connMap.set(conn.name, conn);
-// }
-// console.log(connMap.get('doesnotexist'));
-// console.log(connMap.get('RattleDB'));
-
 function getConnectionMap() {
     const connMap = new Map();
     for(conn of JSON.parse(process.env.CONNECTION_STRINGS)) {
@@ -18,8 +11,6 @@ function getConnectionMap() {
 function getConnection(connName) {
     return getConnectionMap().get(connName);
 }
-
-//console.log(getConnection('RattleDB'));
 
 module.exports = { getConnectionMap, getConnection };
 
